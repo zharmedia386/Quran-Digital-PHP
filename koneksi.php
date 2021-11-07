6e241818c7f89b6daa786d7580dfac521de1d8e2
zharmedia386 <muhammadazharalauddin@gmail.com>
2021-11-07 09:45:05 +0700

    Deployment Heroku

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,15 @@
 
 <?php
 header('Content-Type: text/html; charset=utf-8');
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "dbquran";
+// Deployment Connection
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "dbquran";
 
+// Heroku Deployment
+$server = "remotemysql.com";
+$user = "NINsC6rhHd";
+$password = "3OypXumtTR";
+$database = "NINsC6rhHd";
 $koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error(($koneksi)));
